9a5997b6e1fe6c15b1e32e05ab78a94f56a4732c
minottopaulo <contatominotto@gmail.com>
2025-09-22 20:53:20 -0300

    primeiro commit

---
+++ meu_site/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/servicos")
+def servicos():
+    return render_template("servicos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
